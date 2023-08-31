80f8386a81a7cc50a2b879c5f98c2e1aef2365ac
uraniumrainbow <uraniumrainbow@gmail.com>
2023-08-30 20:19:13 -0600

    fetch request

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+//API Key
+// 3165a95f6d12b7305ddd0dfd9f22e34f
+
 //Globals
 var $SearchBtn = $('#SearchBtn');
 var $ClearBtn = $('#ClearBtn');
@@ -10,3 +13,29 @@ var index = document.querySelector('#index');
 var createImg = document.createElement('img');
 var recentCities = $(JSON.parse(localStorage.getItem('saved')));
 
+function addEntry() {
+    recentCities.sort();
+    for(let i=0; i < recentCities.length; i++){
+
+    }
+};
+
+$SearchBtn.on('click', function(event){
+    console.log(input.value);
+    }
+);
+
+//fetch request
+fetch (`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=3165a95f6d12b7305ddd0dfd9f22e34f`)
+    .then(response => response.json())
+    .then(data => {
+        var cityVal = data['name'];
+        var tempVal = data['main']['temp'];
+        var windVal = data['wind']['speed'];
+        var humidVal = data['main']['humidity'];
+
+        cityName.innerHTML = (cityVal + ' ');
+        temp.innerHTML = (`Temperature: ${tempVal}°C`);
+        wind.innerHTML = (`Wind Speed : ${windVal} mph`);
+        humidity.innerHTML = (`Humidity: ${humidVal}`);
+    })
